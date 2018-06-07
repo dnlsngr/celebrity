3cec9ee1742db9398ea791361b17aaef02a4da71
Daniel Singer <use@myusername.com>
2018-06-07 09:01:14 -0400

    fix text for illegal clue button

--- src/pages/gameplay/play-round/index.tsx
+++ src/pages/gameplay/play-round/index.tsx
@@ -64,7 +64,7 @@ export class PlayRound extends React.Component<PlayRoundProps, {}> {
           data-test="illegal-clue-button"
           onClick={this.props.illegalClue}
         >
-          Illegal Clue (Right-Arrow)
+          Illegal Clue (X)
         </Button>
       </div>
     );
